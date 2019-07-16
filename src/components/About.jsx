10acf4890ded68f887455635856999d3631eff11
vasilisa <igorevna.vasilisa@gmail.com>
2019-07-16 16:36:52 +0200

    desktop pages

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,17 +4,43 @@ import { YMaps, Map, Placemark } from 'react-yandex-maps'
 import Link from '../helpers/Link'
 import Slider from 'react-slick'
 
-import placeholder from '../assets/placeholder.png'
+import placeholder from '../assets/placeholder_2.png'
 
 export default class extends Component {
+  constructor (props) {
+    super(props)
+
+    const height = (window.innerWidth < 984) ? '100%' : '410px'
+
+    this.state = {
+      mapHeight: height
+    }
+  }
+
+  componentDidMount () {
+    document.addEventListener('resized', () => {
+      if (window.innerWidth < 984 && this.state.mapHeight === '410px') {
+        this.setState({
+          mapHeight: '100%'
+        })
+      } else if (window.innerWidth >= 984 && this.state.mapHeight === '100%') {
+        this.setState({
+          mapHeight: '410px'
+        })
+      }
+    })
+  }
+
   render () {
     const settings = {
       dots: false,
       arrows: false,
       infinite: true,
-      speed: 500,
       slidesToShow: 1,
-      slidesToScroll: 1
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 5000,
+      speed: 1000
     }
 
     return (
@@ -29,16 +55,18 @@ export default class extends Component {
         <div className='about-inner'>
           <div className='about-title--wrap'>
             <div className='about-title--name'>Fields <br/>2019</div>
-            <div className='about-title--date'>10 — 11 августа</div>
+            <div className='about-title--date'>10 — 11 <br/>августа</div>
           </div>
-          <div className='about-text--wrap'>
+          <div className='about-text--address'>
             <div className='about-text--mutabor'>
               <svg width="100%" height="100%" viewBox="0 0 28 52" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M26.2262 0V27.4925C25.886 37.0437 18.1426 40.5893 14.0154 40.3342H13.9314C9.5424 40.266 2.8224 37.4248 1.7738 28.0264V0H0V52H1.7738V37.3373C3.2564 38.5474 7.6062 41.8393 13.8138 41.8393C20.328 41.8393 24.8472 38.577 26.2458 37.3403L26.2262 41.8171H28V0H26.2262Z" fill="black"/>
               </svg>
             </div>
+            <p><span>АДРЕС: <br/></span>Москва, <br/>Шарикоподшипниковская ул, <br/>д. 13, с32</p>
+          </div>
+          <div className='about-text--wrap'>
             <div className='about-text--self'>
-              <p>Москва, <br/>Шарикоподшипниковская ул, <br/>д. 13, с32</p>
               <p>Пятый выпуск фестиваля изобретательной музыки Fields пройдет в новом пространстве команд Arma и «Рабица» — Mutabor. Его программа охватит временной интервал в два дня и одну ночь, а само действо развернется на трех параллельных сценах.</p>
               <p>В программе: десяток с лишним зарубежных гостей, свыше 30 живых выступлений, долгожданные российские премьеры и внезапные коллаборации. Кураторское начало, как и прежде, превалирует над трендами: важная роль на фестивале Fields отводится неочевидным героям независимой сцены и серым кардиналам авангарда.</p>
               <div className='about-text--slider-wrap'>
@@ -46,25 +74,21 @@ export default class extends Component {
                   <Slider {...settings}>
                     <div className='about-text--slider-item'>
                       <img alt='placeholder' src={placeholder}/>
-                      <span style={{position: 'absolute', top: '0', left: '0'}}>1</span>
+                      <span style={{color: '#000', top: '50%', left: '50%', transform: 'translate(-50%,-50%)', position: 'absolute'}}>1</span>
                     </div>
                     <div className='about-text--slider-item'>
                       <img alt='placeholder' src={placeholder}/>
-                      <span style={{position: 'absolute', top: '0', left: '0'}}>2</span>
+                      <span style={{color: '#000', top: '50%', left: '50%', transform: 'translate(-50%,-50%)', position: 'absolute'}}>2</span>
                     </div>
                     <div className='about-text--slider-item'>
                       <img alt='placeholder' src={placeholder}/>
-                      <span style={{position: 'absolute', top: '0', left: '0'}}>3</span>
+                      <span style={{color: '#000', top: '50%', left: '50%', transform: 'translate(-50%,-50%)', position: 'absolute'}}>3</span>
                     </div>
                     <div className='about-text--slider-item'>
                       <img alt='placeholder' src={placeholder}/>
-                      <span style={{position: 'absolute', top: '0', left: '0'}}>4</span>
+                      <span style={{color: '#000', top: '50%', left: '50%', transform: 'translate(-50%,-50%)', position: 'absolute'}}>4</span>
                     </div>
                   </Slider>
-                </div>
-                <div className='about-text--slider-desc'>
-                  <div className='about-text--slider-desc-top'>Описание/ Копирайт</div>
-                  <div className='about-text--slider-desc-bottom'>Photo Description / Copyright</div>
                 </div>
               </div>
               <p>В этом году, помимо основной программы, фестиваль включает кураторские блоки и шоукейсы, равномерно распыленные по программной сетке. За их формирование отвечают музыкальные сообщества, звукозаписывающие лейблы и тематические музыкальные медиа: Diagonal Records, The Wire Soundsystem, Рихтерфест, Ored Recordings, New New World Radio.</p>
@@ -82,10 +106,13 @@ export default class extends Component {
             </Link>
           </div>
           <div className='about-map--wrap'>
+            <div className='about-extra-text--wrap'>
+              <p>На проятежении своей истории Fields был уличным, парковым open-air событием. В этом году мы пробуем много новых форматов, в том числе и фестиваль на закрытой локации — Mutabor</p>
+            </div>
             <YMaps>
               <Map
                 width='100%'
-                height='100%'
+                height={this.state.mapHeight}
                 defaultState={{
                   center: [55.7196599, 37.6865011],
                   zoom: 15,
@@ -98,7 +125,7 @@ export default class extends Component {
           <div className='about-link--prev'>
             <Link className='about-link' to='/lineup'>
               <div className='about-link--arrow'>
-                <svg width="92" height="35" viewBox="0 0 92 35" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <svg width="100%" height="100%" viewBox="0 0 92 35" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M19.7024 33.959L0.999963 17.0908L19.7024 0.540844" stroke="white"/>
                   <path d="M0.999979 17.0908L91.9325 17.0908" stroke="white"/>
                 </svg>
--- src/components/History.jsx
+++ src/components/History.jsx
@@ -2,18 +2,121 @@ import React, { Component } from 'react'
 import { Helmet } from 'react-helmet'
 import Link from '../helpers/Link'
 import Slider from 'react-slick'
+import Carousel, { Modal, ModalGateway } from 'react-images'
 
-import placeholder from '../assets/placeholder.png'
+import history2014_1 from '../assets/2014/s3564.jpg'
+import history2014_2 from '../assets/2014/s3593.jpg'
+import history2014_3 from '../assets/2014/s3642.jpg'
+import history2014_4 from '../assets/2014/s3648.jpg'
+import history2014_5 from '../assets/2014/s3684.jpg'
+import history2014_6 from '../assets/2014/s3756.jpg'
+import history2014_7 from '../assets/2014/s3985.jpg'
+import history2014_8 from '../assets/2014/s3877.jpg'
+import history2014_9 from '../assets/2014/s3940.jpg'
+import history2014_10 from '../assets/2014/s3757.jpg'
+
+import history2015_1 from '../assets/2015/1504947_757340464376406_1381679919568075176_n.jpg'
+import history2015_2 from '../assets/2015/1509082_757340171043102_4720349285881380883_n.jpg'
+import history2015_3 from '../assets/2015/1970525_757340237709762_5419463760159070346_n.jpg'
+import history2015_4 from '../assets/2015/10407274_757340261043093_7853775861903639736_n.jpg'
+import history2015_5 from '../assets/2015/11058727_757340284376424_7180374392281188603_n.jpg'
+import history2015_6 from '../assets/2015/11702831_757340017709784_3748575309715643114_n.jpg'
+import history2015_7 from '../assets/2015/11953231_757340351043084_337065777504997676_n.jpg'
+import history2015_8 from '../assets/2015/11954641_757340147709771_9147964476891856882_n.jpg'
+import history2015_9 from '../assets/2015/11954657_757340191043100_1048266910621087750_n.jpg'
+import history2015_10 from '../assets/2015/11987017_757339997709786_2675335726089717320_n.jpg'
+
+import history2016_1 from '../assets/2016/14257622_10154588819936385_8791105463555927041_o.jpg'
+import history2016_2 from '../assets/2016/14249858_1120401331329339_1433518545623849095_o.jpg'
+import history2016_3 from '../assets/2016/14310503_1120401661329306_904858933304630698_o.jpg'
+import history2016_4 from '../assets/2016/pr_0aaaa923a7b1aba125498272ef475224.jpg'
+import history2016_5 from '../assets/2016/pr_39ca8a2125eed88f8644839358c1de45.jpg'
+import history2016_6 from '../assets/2016/GG2A1521.jpg'
+import history2016_7 from '../assets/2016/GG2A2213.jpg'
+import history2016_8 from '../assets/2016/GG2A2531.jpg'
+import history2016_9 from '../assets/2016/14324635_1120401867995952_662339614431447010_o.jpg'
+import history2016_10 from '../assets/2016/14242283_1120402141329258_4992477523785390084_o.jpg'
+
+import history2018_1 from '../assets/2018/Filds-6543.jpg'
+import history2018_2 from '../assets/2018/Filds-6786.jpg'
+import history2018_3 from '../assets/2018/Filds-6916.jpg'
+import history2018_4 from '../assets/2018/Filds-7225.jpg'
+import history2018_5 from '../assets/2018/Filds-7315.jpg'
+import history2018_6 from '../assets/2018/Filds-7561.jpg'
+import history2018_7 from '../assets/2018/Filds-7567.jpg'
+import history2018_8 from '../assets/2018/Filds-7592.jpg'
+import history2018_9 from '../assets/2018/Filds-7658.jpg'
+import history2018_10 from '../assets/2018/Filds-6567.jpg'
+
+const images = [
+  { src: history2014_1 },
+  { src: history2014_2 },
+  { src: history2014_3 },
+  { src: history2014_4 },
+  { src: history2014_5 },
+  { src: history2014_6 },
+  { src: history2014_7 },
+  { src: history2014_8 },
+  { src: history2014_9 },
+  { src: history2015_1 },
+  { src: history2015_2 },
+  { src: history2015_3 },
+  { src: history2015_4 },
+  { src: history2015_5 },
+  { src: history2015_6 },
+  { src: history2015_7 },
+  { src: history2015_8 },
+  { src: history2015_9 },
+  { src: history2016_1 },
+  { src: history2016_2 },
+  { src: history2016_3 },
+  { src: history2016_4 },
+  { src: history2016_5 },
+  { src: history2016_6 },
+  { src: history2016_7 },
+  { src: history2016_8 },
+  { src: history2016_9 },
+  { src: history2018_1 },
+  { src: history2018_2 },
+  { src: history2018_3 },
+  { src: history2018_4 },
+  { src: history2018_5 },
+  { src: history2018_6 },
+  { src: history2018_7 },
+  { src: history2018_8 },
+  { src: history2018_9 },
+]
 
 export default class extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      modalIsOpen: false,
+      currentIndex: 0
+    }
+
+    this.toggleModal = this.toggleModal.bind(this)
+  }
+
+  toggleModal = (id) => {
+    this.setState(state => ({
+      modalIsOpen: !state.modalIsOpen,
+      currentIndex: id
+    }))
+  }
+
   render () {
     const settings = {
       dots: false,
       arrows: false,
       infinite: true,
-      speed: 500,
       slidesToShow: 1,
-      slidesToScroll: 1
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 5000,
+      speed: 1000,
+      variableHeight: true,
     }
 
     return (
@@ -30,26 +133,18 @@ export default class extends Component {
             <div className='history-slider'>
               <Slider {...settings}>
                 <div className='about-text--slider-item'>
-                  <img alt='placeholder' src={placeholder}/>
-                  <span style={{position: 'absolute', top: '0', left: '0'}}>1</span>
+                  <img alt='placeholder' src={history2014_10}/>
                 </div>
                 <div className='about-text--slider-item'>
-                  <img alt='placeholder' src={placeholder}/>
-                  <span style={{position: 'absolute', top: '0', left: '0'}}>2</span>
+                  <img alt='placeholder' src={history2015_10}/>
                 </div>
                 <div className='about-text--slider-item'>
-                  <img alt='placeholder' src={placeholder}/>
-                  <span style={{position: 'absolute', top: '0', left: '0'}}>3</span>
+                  <img alt='placeholder' src={history2016_10}/>
                 </div>
                 <div className='about-text--slider-item'>
-                  <img alt='placeholder' src={placeholder}/>
-                  <span style={{position: 'absolute', top: '0', left: '0'}}>4</span>
+                  <img alt='placeholder' src={history2018_10}/>
                 </div>
               </Slider>
-            </div>
-            <div className='history-slider--desc'>
-              <div className='history-slider--desc-top'>Год / Описание фото / Копирайт</div>
-              <div className='history-slider--desc-bottom'>Year / Photo Description / Copyright</div>
             </div>
           </div>
           <div className='history-text--wrap'>
@@ -61,6 +156,35 @@ export default class extends Component {
                 <p>Фестиваль Fields пришел в Москву в 2014 году. Местом проведения стал парк искусств «Музеон», совсем недавно прошедший этап реновации. Получивший поддержку московского Департамента Культуры, фестиваль поставил перед собой амбициозную цель — реформировать сложившуюся практику городских мероприятий, сделав упор на новое и неизведанное. Сохраняя свободный вход для всех желающих, фестиваль предложил горожанам окунуться в мир изобретательной музыки и авангарда.</p>
                 <p>Описывая идею первого выпуска, фестиваль ссылался на слоган культового британского журнала об авангардной музыке The Wire: «Adventures In Sound And Music» («Приключения в звуке и музыке»). Хедлайнерами фестиваля стали берлинский неоклассический пианист Hauschka, исландская виолончелистка Хильдур Гуднадоттир (Hildur Guðnadóttir), редкое трио американского композитора-минималиста Шарлеманя Палестина, финского нойз-импровизатора Мики Вайнио и бельгийского перкуссиониста Эрика Тилеманса, а также британский мультиинструменталист Talvihorros.</p>
                 <p>Локальную сцену представили не менее важные герои: Антон Маскелиаде, новосибирский эмбиент-фолк-проект Foresteppe, екатеринбургская группа «4 Позиции Бруно», московский фри джаз бэнд «Бром» и многие другие.</p>
+                <div className='history-text--pictures-wrap'>
+                  <div className='history-text--pictures-single' onClick={ev => this.toggleModal(0)} >
+                    <img alt='2014' src={history2014_1} />
+                  </div>
+                  <div className='history-text--pictures-single' onClick={ev => this.toggleModal(1)} >
+                    <img alt='2014' src={history2014_2} />
+                  </div>
+                  <div className='history-text--pictures-single' onClick={ev => this.toggleModal(2)} >
+                    <img alt='2014' src={history2014_3} />
+                  </div>
+                  <div className='history-text--pictures-single' onClick={ev => this.toggleModal(3)} >
+                    <img alt='2014' src={history2014_4} />
+                  </div>
+                  <div className='history-text--pictures-single' onClick={ev => this.toggleModal(4)} >
+                    <img alt='2014' src={history2014_5} />
+                  </div>
+                  <div className='history-text--pictures-single' onClick={ev => this.toggleModal(5)} >
+                    <img alt='2014' src={history2014_6} />
+                  </div>
+                  <div className='history-text--pictures-single' onClick={ev => this.toggleModal(6)} >
+                    <img alt='2014' src={history2014_7} />
+                  </div>
+                  <div className='history-text--pictures-single' onClick={ev => this.toggleModal(7)} >
+                    <img alt='2014' src={history2014_8} />
+                  </div>
+                  <div className='history-text--pictures-single' onClick={ev => this.toggleModal(8)} >
+                    <img alt='2014' src={history2014_9} />
+                  </div>
+                </div>
               </div>
               <div className='history-text--block'>
                 <div className='history-text--year'>2015</div>
@@ -69,6 +193,35 @@ export default class extends Component {
                 <p>На главной сцене состоялись выступления британского фолк-композитора Джеймса Блэкшо, норвежского фьюжн-гитариста Стайана Урхайма, российской электронной дивы Ishome и многих других. Знаковой премьерой стало выступление британского дуэта patten, который представил эпическое аудиовизуальное шоу с лазерами.</p>
                 <p>Важным событием стала экспериментальная программа фестиваля, состоявшаяся на отдельной сцене. Британский электронный авангардист Марк Фелл представил совместную программу с итальянским виолончелистом Сандро Муссидой, а берлинский музыкант и звукоинженер Рашад Беккер представил свой модулярный лайв-перформанс.</p>
                 <p>На третьей сцене состоялся отдельный шоукейс лейбла Fancy Music, который представил срез современного российского джаза.</p>
+                <div className='history-text--pictures-wrap'>
+                  <div className='history-text--pictures-single' onClick={ev => this.toggleModal(9)}>
+                    <img alt='2015' src={history2015_1} />
+                  </div>
+                  <div className='history-text--pictures-single' onClick={ev => this.toggleModal(10)}>
+                    <img alt='2015' src={history2015_2} />
+                  </div>
+                  <div className='history-text--pictures-single' onClick={ev => this.toggleModal(11)}>
+                    <img alt='2015' src={history2015_3} />
+                  </div>
+                  <div className='history-text--pictures-single' onClick={ev => this.toggleModal(12)}>
+                    <img alt='2015' src={history2015_4} />
+                  </div>
+                  <div className='history-text--pictures-single' onClick={ev => this.toggleModal(13)}>
+                    <img alt='2015' src={history2015_5} />
+                  </div>
+                  <div className='history-text--pictures-single' onClick={ev => this.toggleModal(14)}>
+                    <img alt='2015' src={history2015_6} />
+                  </div>
+                  <div className='history-text--pictures-single' onClick={ev => this.toggleModal(15)}>
+                    <img alt='2015' src={history2015_7} />
+                  </div>
+                  <div className='history-text--pictures-single' onClick={ev => this.toggleModal(16)}>
+                    <img alt='2015' src={history2015_8} />
+                  </div>
+                  <div className='history-text--pictures-single' onClick={ev => this.toggleModal(17)}>
+                    <img alt='2015' src={history2015_9} />
+                  </div>
+                </div>
               </div>
               <div className='history-text--block'>
                 <div className='history-text--year'>2016</div>
@@ -76,6 +229,35 @@ export default class extends Component {
                 <p>Третий выпуск фестиваля не стал ограничиваться одним парком — программа фестиваля распределилась равномерно между уже традиционным «Музеоном» и более камерным Садом Баумана.</p>
                 <p>В рамках основной программы фестиваля в «Музеоне» состоялось несколько знаковых премьер. Аргентинский электронный музыкант Murcof и французская пианистка Ванесса Вагнер исполнили интерпретации работ композиторов 20 века: от Эрика Сати и Арво Пярта до Брайана Ино и Aphex Twin. Впервые в России выступил легендарный фолк-музыкант Майк Купер, представивший свою лайв-программу с электроникой. Лондонский продюсер Heatsick представил свое видение лоуфайного хауса, исполнив живой перформанс лишь на одном синтезаторе Casio и мараканах.</p>
                 <p>Тем временем в Саду Баумана состоялась камерная программа Piano FIelds, ориентированная на передовую фортепианную музыку. Здесь выступили американский джазовый пианист Джейми Сафт, французский эмбиент-музыкант Сильвэ Шово, неоклассический пианист Миша Мищенко, композиторы-минималисты Владимир Мартынов и Петру Айду.</p>
+                <div className='history-text--pictures-wrap'>
+                  <div className='history-text--pictures-single' onClick={ev => this.toggleModal(18)}>
+                    <img alt='2016' src={history2016_1} />
+                  </div>
+                  <div className='history-text--pictures-single' onClick={ev => this.toggleModal(19)}>
+                    <img alt='2016' src={history2016_2} />
+                  </div>
+                  <div className='history-text--pictures-single' onClick={ev => this.toggleModal(20)}>
+                    <img alt='2016' src={history2016_3} />
+                  </div>
+                  <div className='history-text--pictures-single' onClick={ev => this.toggleModal(21)}>
+                    <img alt='2016' src={history2016_4} />
+                  </div>
+                  <div className='history-text--pictures-single' onClick={ev => this.toggleModal(22)}>
+                    <img alt='2016' src={history2016_5} />
+                  </div>
+                  <div className='history-text--pictures-single' onClick={ev => this.toggleModal(23)}>
+                    <img alt='2016' src={history2016_6} />
+                  </div>
+                  <div className='history-text--pictures-single' onClick={ev => this.toggleModal(24)}>
+                    <img alt='2016' src={history2016_7} />
+                  </div>
+                  <div className='history-text--pictures-single' onClick={ev => this.toggleModal(25)}>
+                    <img alt='2016' src={history2016_8} />
+                  </div>
+                  <div className='history-text--pictures-single' onClick={ev => this.toggleModal(26)}>
+                    <img alt='2016' src={history2016_9} />
+                  </div>
+                </div>
               </div>
               <div className='history-text--block'>
                 <div className='history-text--year'>2018</div>
@@ -83,6 +265,35 @@ export default class extends Component {
                 <p>После паузы в год фестиваль Fields вернулся. В очередной раз он предложил горожанам альтернативный формат Дня города, а локацией фестиваля на этот раз стал главное городское пространство для культуры и отдыха — Парк Горького.</p>
                 <p>Четвертый выпуск фестиваля Fields с подстрочником Open Borders стирал всевозможные границы — географические, стилистические, культурные и субкультурные. Фолк через призму рейва, стадионный рок на игрушечных инструментах, электронная музыка в интерпретации академистов, техно на ударной установке из 60 барабанов и нью-эйдж 21-го века. Среди главных привозных актов оказалось выступления американской легенды эмбиента Laraaji, а также египетского музыканта и клавишника Ислам Чипси, который вместе со своей группой представил собственное видение стиля электро-чааби.</p>
                 <p>Российскую сцену представили не менее важные проекты. На фестивале состоялась внезапная коллаборация одного из главных российских техно-диджеев Никиты Забелина и композитора, правнука основателя терменвокса Петра Термена. Ученый-химик Алексей Бобровский исполнил акустический техно-перформанс на гигантской ударной установке, а проект АДМИ представил лайв на детских игрушках. Ансамбль академических музыкантов Kymatic исполнил пьесы современных электронных музыкантов, а трио Fogh Depot разрушило границы между джазом, неоклассикой и электроникой.</p>
+                <div className='history-text--pictures-wrap'>
+                  <div className='history-text--pictures-single' onClick={ev => this.toggleModal(27)}>
+                    <img alt='2018' src={history2018_1} />
+                  </div>
+                  <div className='history-text--pictures-single' onClick={ev => this.toggleModal(28)}>
+                    <img alt='2018' src={history2018_2} />
+                  </div>
+                  <div className='history-text--pictures-single' onClick={ev => this.toggleModal(29)}>
+                    <img alt='2018' src={history2018_3} />
+                  </div>
+                  <div className='history-text--pictures-single' onClick={ev => this.toggleModal(30)}>
+                    <img alt='2018' src={history2018_4} />
+                  </div>
+                  <div className='history-text--pictures-single' onClick={ev => this.toggleModal(31)}>
+                    <img alt='2018' src={history2018_5} />
+                  </div>
+                  <div className='history-text--pictures-single' onClick={ev => this.toggleModal(32)}>
+                    <img alt='2018' src={history2018_6} />
+                  </div>
+                  <div className='history-text--pictures-single' onClick={ev => this.toggleModal(33)}>
+                    <img alt='2018' src={history2018_7} />
+                  </div>
+                  <div className='history-text--pictures-single' onClick={ev => this.toggleModal(34)}>
+                    <img alt='2018' src={history2018_8} />
+                  </div>
+                  <div className='history-text--pictures-single' onClick={ev => this.toggleModal(35)}>
+                    <img alt='2018' src={history2018_9} />
+                  </div>
+                </div>
               </div>
             </div>
           </div>
@@ -98,6 +309,13 @@ export default class extends Component {
             </Link>
           </div>
         </div>
+        <ModalGateway>
+          { this.state.modalIsOpen ? (
+            <Modal onClose={this.toggleModal}>
+              <Carousel trackProps={{infinite: 'true'}} currentIndex={this.state.currentIndex} views={images} />
+            </Modal>
+          ) : null}
+        </ModalGateway>
       </div>
     )
   }
